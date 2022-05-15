2f558a39781919a9e75f64df5c766d4ea3fa2b8d
Jun <154179code@gmail.com>
2022-05-15 11:00:53 -0600

    links mod and index file added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
-import { AboutPage } from "../01-lazyload/Pages/AboutPage";
-import { HomePage } from "../01-lazyload/Pages/HomePage";
-import { UsersPage } from "../01-lazyload/Pages/UsersPage";
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom";
+import { UsersPage, AboutPage } from "../01-lazyload/Pages";
+import { ShoppingPages } from "../02-components-patterns/pages/ShoppingPages";
 import logo from "../logo.svg";
 export const Navigation = () => {
     return (
         <BrowserRouter>
             <div className="main-layout">
                 <nav>
-                    <img src={logo} />
+                    <img src={logo} alt="React Logo" />
                     <ul>
-                        <li><NavLink to='/home'>Home</NavLink></li>
-                        <li><NavLink to='/about'>About</NavLink></li>
-                        <li><NavLink to='/users'>Users</NavLink></li>
+                        <li><NavLink to='/' className={({ isActive }) => (isActive ? "nav-active" : "")}>Shopping</NavLink></li>
+                        <li><NavLink to='/about' className={({ isActive }) => (isActive ? "nav-active" : "")}>About</NavLink></li>
+                        <li><NavLink to='/users' className={({ isActive }) => (isActive ? "nav-active" : "")}>Users</NavLink></li>
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path='home' element={<HomePage />}/>
-                    <Route path='about' element={<AboutPage />}/>
-                    <Route path='users' element={<UsersPage />}/>
+                    <Route path='/' element={<ShoppingPages />} />
+                    <Route path='about' element={<AboutPage />} />
+                    <Route path='users' element={<UsersPage />} />
+                    <Route path="/*" element={<Navigate to="/home" />} />
                 </Routes>
             </div>
         </BrowserRouter>
